1fc53a17740cd3e0cebb7e0dcf9334f88ab3695d
Hatsumi Hayashi <hayashihatsumi0114@gmail.com>
2022-08-28 21:57:24 +0900

    先にテストを日本語化

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -18,23 +18,23 @@ class QuotesTest < ApplicationSystemTestCase
 
   test "Updating a quote" do
     visit quotes_path
-    assert_selector "h1", text: "Quotes"
+    assert_selector "h1", text: "見積り一覧"
 
-    click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
+    click_on "更新", match: :first
+    assert_selector "h1", text: "見積りを更新"
 
-    fill_in "Name", with: "Updated quote"
-    click_on "Update quote"
+    fill_in "見積りのタイトル", with: "見積りをあたらしいNameに更新"
+    click_on "見積りを更新"
 
-    assert_selector "h1", text: "Quotes"
-    assert_text "Updated quote"
+    assert_selector "h1", text: "見積り一覧"
+    assert_text "見積りをあたらしいNameに更新"
   end
 
   test "Destroying a quote" do
     visit quotes_path
     assert_text @quote.name
 
-    click_on "Delete", match: :first
+    click_on "削除", match: :first
     assert_no_text @quote.name
   end
 end
