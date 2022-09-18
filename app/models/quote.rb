cf819cbd5e36c23c496c93a6c6c75333e7804ec7
Hatsumi Hayashi <hayashihatsumi0114@gmail.com>
2022-09-18 18:31:45 +0900

    [チュートリアル] 見積りに`LineItemDate`へのリレーションを追加

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,8 +1,9 @@
 class Quote < ApplicationRecord
   belongs_to :company
-  
+  has_many :line_item_dates, dependent: :destroy
+
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
 
-  broadcasts_to ->(quote) { [quote.company, "quotes"]  }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
